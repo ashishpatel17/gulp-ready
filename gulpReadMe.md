c0701f0ad76752c6391ac33842535a3b95f76dcc
ashish patel <mpatelashish@gmail.com>
2016-12-15 12:58:50 +0530

    New Features   1)  Add gzipping Features   2) Image optimizaion   3) new config parameter for specifying ignore list   4) processed only changed files while executing watcher
    Bug Resolve
      1) Resolve bug of gulpbuid duplicate directory creation

--- gulpReadMe.md
+++ gulpReadMe.md
@@ -6,7 +6,7 @@ gulp is the node based tool which is used to automate the several repetitive tas
 
 About
 -------------
-To simplify the process of using gulp here i had try to creating the medium as a config file , where user will specify the required configs in json file and based on that it gulp tasks will generate the output files
+To simplify the process of using gulp and try to creating the medium as a config file where user will specify the required configs in json file and based on that it gulp tasks will generate the output files
 
 Task Provided
 -------------
@@ -14,34 +14,35 @@ Task Provided
   - minifyCss - generate minified version of css file of source directory
   - generateDeploymentFiles -  generate the single common js and css file from the minifed version of files
   - Watcher - automated process to keep watch on source directory and on any changes occured regerate the new output files
+  - clear - it will clear the output directory
   - default - single process to execute all of the above process at once
+  - optimizeImage - Optize the image of given path if path not provided in config it will target all directory of currunt path
 
 
 Output Files Folder Structure
 ===================
-This gulp process will create the folder by name "guilpbuild" at specified output Destination and inside that folder it will create 3 folders minifiedFiles , uglifyCss and uglifyJs.
+This gulp process will create the folder by name "guilpbuild" at specified output Destination and inside that folder it will create 4 folders minifiedFiles , optimizedImages , uglifyCss and uglifyJs.
 
 * [gulpBuild]()
  * [minifiedFiles ]()
+ * [optimizedImages ]()
  * [uglifyCss ]()
  *  [uglifyJs]()
 
 ##### minifiedFiles
-	minifedFiles folder have the same directory structure as source path ,it holds similar folder and subfolders source 
-	path has and that folder contains the minified version of css and js.
-
-	minifyCss and minifyJs task will do this process
+	minifedFiles folder have the same directory structure as source path ,it holds similar folder and subfolders source path has and
+	that folder contains the minified version of css and js. minifyCss and minifyJs task will do this process
 
 ##### uglifyCss , uglifyJs
 
-	This folder contain the merge uglified version of all css and js files inside the minifiedFiles folder . to generate
-	the uglify file it use the config ,if list of css and js files is specify in config then tasks will generate the 
-	uglify files using that set of files , if not specified then it will merge all css and js exist inside the minifiedFiles
-	folder and generate global.css anf global.js
+	This folder contain the merge uglified version of all css and js files inside the minifiedFiles folder . to generate the uglify file it use the config ,if list of css and js files is specify in config then tasks will generate the uglify files using that set of files , if not specified then it will merge all css and js exist inside the minifiedFiles folder and generate global.css anf global.js
+
+##### optimizedImages
+	 This folder containt the optimized version of images
 
 After all this files are generate use this files at required html pages
 
-
+----------
 
 
 gulpConfig.json
@@ -54,7 +55,10 @@ following parameters are necessary to specify in config file to execute task . g
 {
     "project":"public/Dmaterial/assets",
     "outputDestination":"public/Dmaterial/",
+    "targetImageDirectory":"theme/images/",
+    "ignore":["!node_modules/**/*"],
     "generateGlobalFiles" : false,
+    "enableGzip" : true,
     "css":{
        "global":{
           "files":[
@@ -77,13 +81,17 @@ following parameters are necessary to specify in config file to execute task . g
 ```
 ##### project
 	specify path of the source project here
-##### ignore
-  	specify folders or any file to ignore in array
 ##### outputDestination
 	specify path of the generated output files here
+##### targetImageDirectory
+	 specify path of the target directory of images or list of images which should be selected for optimization process
+##### ignore
+	 list the paths or files to be ignored here path or files specified here will not be processed by any gulp task
+  attach '!' before path other wise it wont be ignored by gulp tasks
+##### enableGzip
+  if true then generate the .gip file with minifedFiles
 ##### css , js
-	To generate the uglify files you can specify the json inside the css. keys will be considered as a name of
-	files and each key values will be array which containe list of files or path of directory.
+	To generate the uglify files you can specify the json inside the css. keys will be considered as a name of files and each key values will be array which containe list of files or path of directory.
 	Eg:  
 	"css":{
 	  "global":{ // create the global.min.css in uglifyCss folder
@@ -91,14 +99,13 @@ following parameters are necessary to specify in config file to execute task . g
 	        "css/bootstrap.css",
 	        "css/scaffolding.css",
 	        "css/zdepth.css",
-	        "css/utilities.css",
+	        "css/utilities.css"
 	        "bootstrap/**/*.css" // target all css files of bootstrap folder and subfolders
 	          ]
 	       }
 
 ##### generateGlobalFiles
-	If true and css or js params is not specified in json then it will generate the merge and minified 
-	global.min.css or global.min.js using all css and js files inside the minifiedFiles folder
+	If true and css or js params is not specified in json then it will generate the merge and minified  global.min.css or global.min.js using all css and js files inside the minifiedFiles folder
 
 
 
@@ -108,7 +115,7 @@ following parameters are necessary to specify in config file to execute task . g
  - Node.js - [<i ></i> nodejs.org](https://nodejs.org/en/)
  - Gulp -  [<i ></i> gulpjs.com](http://gulpjs.com/)
 
-
+  ----------
 
 Dependent Node Modules
 -------------
@@ -116,15 +123,17 @@ Dependent Node Modules
  - path - [<i ></i> nodejs.org](https://nodejs.org/en/)
  - gulp -  [<i ></i> npmjs.com/package/gulp](https://www.npmjs.com/package/gulp)
  - gulp-rename -  [<i ></i> npmjs.com/package/gulp-rename](https://www.npmjs.com/package/gulp-rename)
- - gulp-cssmin -  [<i ></i> npmjs.com/package/gulp-cssmin](https://www.npmjs.com/package/gulp-cssmin)
+ - gulp-cssnano -  [<i ></i> npmjs.com/package/gulp-cssnano](https://www.npmjs.com/package/gulp-cssnano)
  - gulp-uglify -  [<i ></i> npmjs.com/package/gulp-uglify](https://www.npmjs.com/package/gulp-uglify)
  - gulp-concat -  [<i ></i> npmjs.com/package/gulp-concat](https://www.npmjs.com/package/gulp-concat)
  - gulp-plumber -  [<i ></i> npmjs.com/package/gulp-plumber](https://www.npmjs.com/package/gulp-plumber)
  - gulp-watch -  [<i ></i> npmjs.com/package/gulp-watch](https://www.npmjs.com/package/gulp-watch)
  - del -  [<i ></i> npmjs.com/package/del](https://www.npmjs.com/package/del)
  - run-sequence -  [<i ></i> npmjs.com/package/run-sequence](https://www.npmjs.com/package/run-sequence)
-
-
+ -   gulp-smushit -  [<i ></i> npmjs.com/package/gulp-smushit](https://www.npmjs.com/package/gulp-smushit)
+ -  gulp-gzip -  [<i ></i> npmjs.com/package/gulp-gzip](https://www.npmjs.com/package/gulp-gzip)
+ - gulp-changed -  [<i ></i> npmjs.com/package/gulp-changed](https://www.npmjs.com/package/gulp-changed)
+  ----------
 
 
    Authour
--- gulpfile.js
+++ gulpfile.js
@@ -9,40 +9,49 @@ var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
 var del = require('del');
 var runSequence = require('run-sequence');
+var smushit = require('gulp-smushit');
+var gzip = require('gulp-gzip');
+var changed = require('gulp-changed');
 var config = require('./gulpConfig.json');
 
 
-config.outputDestination = (config.outputDestination==undefined)?"gulpBuild/":config.outputDestination+"gulpBuild/";
 
-var targetResource = config.targetResource!=undefined?config.targetResource:"";
-var projectDirectory = config.project + targetResource;
-var gulpOutputDir = config.outputDestination;
+var projectDirectory = (config.project == undefined) ? "**/" : config.project;
+var enableGzip = (config.enableGzip == undefined) ? false : config.enableGzip;
+var gulpOutputDir = (config.outputDestination == undefined) ? "gulpBuild/" : config.outputDestination + "gulpBuild/";
+var targetImageDirectory = (config.targetImageDirectory == undefined) ? projectDirectory : config.targetImageDirectory;
+var ignoreList = config.ignore == undefined ? [] : config.ignore;
+
+/*
+  Output directory paths
+ */
 var minifiedFiles = gulpOutputDir + "minifiedFiles/";
-var uglifyCssPath = gulpOutputDir+"uglifyCss/";
-var uglifyJSPath = gulpOutputDir+"uglifyJs/";
+var optimizedImgPath = gulpOutputDir + "optimizedImages/";
+var uglifyCssPath = gulpOutputDir + "uglifyCss/";
+var uglifyJSPath = gulpOutputDir + "uglifyJs/";
 
-var ignoreList = config.ignore==undefined?[]:config.ignore;
+
+/*
+  by default ignore gulp output directory and gulpfile.js
+ */
+ignoreList.push('!' + gulpOutputDir + '**/*.*', '!gulpfile.js');
 
 /*
   function to read files and send to uglify process
  */
-function minifyAndConcat(fileSet,type){
-    for(var key in fileSet){
-        var fileName = key;
-        if(fileSet[key].files && fileSet[key].files.length>0){
-          if(type == "css"){
-            var files = fileSet[key].files.map(function(p){
-                return minifiedFiles + p.replace(".css",".min.css");
-            })
-            generateCustomUglify(files,key,"css",uglifyCssPath);
-          }else if(type == "js"){
-            var files = fileSet[key].files.map(function(p){
-                return minifiedFiles + p.replace(".js",".min.js");
-            })
-            generateCustomUglify(files,key,"js",uglifyJSPath);
-          }
-        }
-    }
+function minifyAndConcat(fileSet, type) {
+  var destPath = "";
+  if (type == "js") {
+    destPath = uglifyJSPath;
+  } else if (type == "css") {
+    destPath = uglifyCssPath;
+  }
+  for (var key in fileSet) {
+    var files = fileSet[key].files.map(function(p) {
+      return minifiedFiles + p.replace("." + type, ".min." + type);
+    })
+    generateCustomUglify(files, key, type, destPath);
+  }
 }
 
 /*
@@ -53,114 +62,156 @@ function minifyAndConcat(fileSet,type){
  -  type - Type of file css or js
  -  destinationPath - output generated file path
  */
-function generateCustomUglify(files,fileName,type,destinationPath){
-  if(type=="css"){
-      gulp.src(files)
-     .pipe(concat(fileName+'.min.'+type))
-     .pipe(gulp.dest(destinationPath))
-  }else if(type=="js"){
-      gulp.src(files)
-     .pipe(concat(fileName+'.min.'+type))
-     .pipe(gulp.dest(destinationPath))
+function generateCustomUglify(files, fileName, type, destinationPath) {
+  var uglFile = gulp.src(files)
+    .pipe(concat(fileName + '.min.' + type))
+    .pipe(gulp.dest(destinationPath))
+
+  if (enableGzip) {
+    uglFile.pipe(gzip({
+      append: true
+    })).pipe(gulp.dest(destinationPath))
   }
+  return uglFile;
 }
 
 /*
   - Automated Gulp watcher will watch the changes made to source file and re generate the output file if any chanes is occured
  */
-gulp.task('watcher',function(){
+gulp.task('watcher', function() {
   // watcher that watch any changes made to minifiedFiles folder if done then execute the generateDeploymentFiles
-  gulp.watch([minifiedFiles+'**/*.*'],['generateDeploymentFiles'])
+  gulp.watch([minifiedFiles + '**/*.*'], ['generateDeploymentFiles']);
 
   // if changes done to source file
-  gulp.watch([projectDirectory+'**/*.*','!'+gulpOutputDir+'**/*.*'].concat(ignoreList))
-  .on("change",function(file){
-    //if deleted then get the path of source file and output file and sync the delete process to delete that output file
-    if(file.type=="deleted"){
+  gulp.watch([projectDirectory + '**/*.*'].concat(ignoreList))
+    .on("change", function(file) {
+      // if deleted then get the path of source file and output file and sync the delete process to delete that output
+      if (file.type == "deleted") {
         var filePathFromSrc = path.relative(path.resolve(projectDirectory), file.path);
-        if(filePathFromSrc.indexOf(".css")>-1){
-            filePathFromSrc=filePathFromSrc.replace(".css",".css");
-        }else if(filePathFromSrc.indexOf(".js")>-1){
-            filePathFromSrc=filePathFromSrc.replace(".js",".js");
+        filePathFromSrc = filePathFromSrc.substring(filePathFromSrc.indexOf("..\\") + 3, filePathFromSrc.length);
+        if (filePathFromSrc.indexOf(".css") > -1) {
+          filePathFromSrc = filePathFromSrc.replace(".css", ".min.css");
+        } else if (filePathFromSrc.indexOf(".js") > -1) {
+          filePathFromSrc = filePathFromSrc.replace(".js", ".min.js");
         }
-        var destFilePath = path.resolve(minifiedFiles+targetResource, filePathFromSrc);
+        var destFilePath = path.resolve(minifiedFiles, filePathFromSrc);
+        var destFilePathGZ = path.resolve(minifiedFiles, filePathFromSrc + ".gz");
+
         del.sync(destFilePath);
-    }
-    //if changed or new file created then re generate the minifyCss and minifyJs
-    //on regerating these files above watcher is also executed because changes in minifiedFiles folder is detected
-    else{
-        if(file.path.indexOf(".css")>-1){
-            gulp.start('minifyCss');
-        }else if(file.path.indexOf(".js")>-1){
-            gulp.start('minifyJs');
+        del.sync(destFilePathGZ);
+      }
+      // if changed or new file created then re generate the minifyCss and minifyJs
+      // on regerating these files above watcher is also executed because changes in minifiedFiles folder is detected
+      else {
+        if (file.path.indexOf(".css") > -1) {
+          console.log("test");
+          gulp.start('minifyCss');
+        } else if (file.path.indexOf(".js") > -1) {
+          gulp.start('minifyJs');
         }
-    }
-  })
+      }
+    })
 });
 
 /*
   - Task to generate the uglified version of js and css using minified file from gulpBuild/minifiedFiles folder
  */
-gulp.task('generateDeploymentFiles',function(){
-    // if css and js file is specified the generate the single uglify version of listed files or folder
-    if(config.css && config.js){
-      for(var key in config){
-          if(Object.keys(config[key]).length>0 && (key=="js" || key=="css")){
-            minifyAndConcat(config[key],key);
-          }
+gulp.task('generateDeploymentFiles', function() {
+  // if css and js file is specified the generate the single uglify version of listed files or folder
+  if (config.css || config.js) {
+    for (var key in config) {
+      if (Object.keys(config[key]).length > 0 && (key == "js" || key == "css")) {
+        minifyAndConcat(config[key], key);
       }
     }
+  }
 
-    //if generateGlobalFiles is true in config generate single uglify file of all files inside the minifiedFiles folder
-    if(config.generateGlobalFiles){
-      if(config.css && config.js==undefined){
-          generateCustomUglify(minifiedFiles+'**/*.js',"global","js",uglifyJSPath);
-      }else if(config.css==undefined && config.js){
-          generateCustomUglify(minifiedFiles+'**/*.css',"global","css",uglifyCssPath);
-      }else if(config.css==undefined && config.js==undefined){
-          generateCustomUglify(minifiedFiles+'**/*.js',"global","js",uglifyJSPath);
-          generateCustomUglify(minifiedFiles+'**/*.css',"global","css",uglifyCssPath);
-      }
+  //if generateGlobalFiles is true in config generate single uglify file of all files inside the minifiedFiles folder
+  if (config.generateGlobalFiles) {
+    if (config.css && config.js == undefined) {
+      generateCustomUglify(minifiedFiles + '**/*.js', "global", "js", uglifyJSPath);
+    } else if (config.css == undefined && config.js) {
+      generateCustomUglify(minifiedFiles + '**/*.css', "global", "css", uglifyCssPath);
+    } else if (config.css == undefined && config.js == undefined) {
+      generateCustomUglify(minifiedFiles + '**/*.js', "global", "js", uglifyJSPath);
+      generateCustomUglify(minifiedFiles + '**/*.css', "global", "css", uglifyCssPath);
     }
+  }
 })
 
 /*
   - Task to generate the minified version of Js
  */
-gulp.task('minifyCss',function(){
-    return  gulp.src([
-     projectDirectory+'/**/*.css'
-    ].concat(ignoreList))                             // read files
-  //  .pipe(watch(projectDirectory+'/**/*.css'))
-   .pipe(plumber())                                   // prevent process to stop on any error
-   .pipe(cssmin())                                    // minify the css
-   .pipe(rename(function(path) {
-     path.extname = ".min.css";
-   }))                                                // attach the .min.css to file name
-   .pipe(gulp.dest(minifiedFiles+targetResource));    // save to Destination
-})
+gulp.task('minifyCss', function() {
+  var files = [projectDirectory + '**/*.css'].concat(ignoreList)
+  var destination = minifiedFiles;
+  var cssMin = gulp.src(files, {
+      base: "."
+    }) // read files
+    .pipe(changed(destination, {
+      extension: '.min.css'
+    }))
+    .pipe(plumber()) // prevent process to stop on any error
+    .pipe(cssmin()) // minify the css
+    .pipe(rename(function(path) {
+      path.extname = ".min.css";
+    })) // attach the .min.css to file name
+    .pipe(gulp.dest(destination)) // save to Destination
 
+  if (enableGzip) {
+    cssMin.pipe(gzip({
+      append: true
+    })).pipe(gulp.dest(destination))
+  }
+  return cssMin;
+})
 
 /*
   - Task to generate the minified version of js
  */
-gulp.task('minifyJs',function(){
-    return  gulp.src([
-     projectDirectory+'/**/*.js'
-   ].concat(ignoreList))                                // read files
-  //  .pipe(watch(projectDirectory+'/**/*.js'))
-   .pipe(plumber())                                     // prevent process to stop on any error
-   .pipe(uglify())                                     // minify the js
-   .pipe(rename(function(path) {
-     path.extname = ".min.js";
-   }))                                                // attach the .min.js to file name
-   .pipe(gulp.dest(minifiedFiles+targetResource));    // save to Destination
+gulp.task('minifyJs', function() {
+  var files = [projectDirectory + '**/*.js'].concat(ignoreList);
+  var destination = minifiedFiles;
+  var jsMin = gulp.src(files) // read files
+    .pipe(changed(destination, {
+      extension: '.min.js'
+    }))
+    .pipe(plumber()) // prevent process to stop on any error
+    .pipe(uglify()) // minify the js
+    .pipe(rename(function(path) {
+      path.extname = ".min.js";
+    })) // attach the .min.js to file name
+    .pipe(gulp.dest(destination)) // save to Destination
+
+  if (enableGzip) {
+    jsMin.pipe(gzip({
+      append: true
+    })).pipe(gulp.dest(destination))
+  }
+  return jsMin;
+})
+
+
+/*
+    generate the compressed optimized image of given path
+    currently supports only jpg and png format
+ */
+gulp.task('optimizeImage', function() {
+  return gulp.src([
+      projectDirectory + '**/*.{jpg,png}'
+    ].concat(ignoreList))
+    .pipe(smushit())
+    .pipe(gulp.dest(optimizedImgPath));
+})
+
+gulp.task('clear', function() {
+  del.sync(gulpOutputDir);
 })
 
 /*
 - Single task to execute all four task at once
 - first execute minifyCss & minifyJs parallel and after that execute generateDeploymentFiles and watcher
  */
-gulp.task('default',function(){
-    runSequence(['minifyCss','minifyJs'],'generateDeploymentFiles','watcher');
+gulp.task('default', function() {
+  runSequence('clear', ['minifyCss', 'minifyJs'], 'generateDeploymentFiles', 'watcher');
 })
